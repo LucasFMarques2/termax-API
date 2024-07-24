05cdf2e6d4ba630645fbd5e1ebae0f9702718f20
LucasFMarques2 <lucasfreitasm3@hotmail.com>
2024-07-23 22:02:56 -0300

    Adicionando knexfile.js e o diretório knex

---
+++ src/database/knex/migrations/20240724004930_createTags.js
@@ -0,0 +1,10 @@
+
+exports.up = kenex => kenex.schema.createTable("tags", table =>{
+    table.increments("id");
+    table.text("name").notNullable();
+    table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");
+    table.integer("user_id").references("id").inTable("users");
+});
+
+
+exports.down = kenex => kenex.schema.dropTable("tags");
