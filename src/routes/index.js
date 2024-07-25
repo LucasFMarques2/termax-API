4bf93b94f31044795a8062bb5bf4e6dd6b7e5fec
LucasFMarques2 <lucasfreitasm3@hotmail.com>
2024-07-24 23:15:44 -0300

    Show para migration de notas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const {Router} = require("express");
 const userRouter = require("./users.routes");
+const notesRouter = require("./notes.routes");
 
 const router = Router();
 
 router.use("/users", userRouter)
+router.use("/notes", notesRouter)
 
 module.exports = router;
--- src/routes/user.router.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const userRouter = Router();
-
-userRouter.post("/", (req, res) => {
-    res.send("deu bom a rota");
-});
-
-module.exports = userRouter;
