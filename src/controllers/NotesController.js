c56976df9de095fcc2c1fc2df0bc63bafdc5bfe7
Lucas Freitas Marques (CGIS/STIC/UNIC) <lucasfmarques@mec.gov.br>
2024-07-29 14:06:45 -0300

    Adicionando Tags Controller e Notes COntroller

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,52 +1,94 @@
-const knex = require("../database/knex");
+const knex = require("../database/knex")
 
-class NotesController{
-    async create(req, res){
-        const {title, description, tags, links} = req.body;
-        const {user_id} = req.params;
+class NotesController {
+  async create(request, response) {
+    const { title, description, tags, links } = request.body
+    const { user_id } = request.params
 
-        const [note_id] = await knex("notes").insert({
-            title,
-            description,
-            user_id
-        });
+    const [note_id] = await knex("notes").insert({
+      title,
+      description,
+      user_id
+    })
 
-        const linksInsert = links.map(link =>{
-            return{
-                note_id,
-                url: link
-            }
-        });
+    const linksInsert = links.map(link => {
+      return {
+        note_id,
+        url: link
+      }
+    })
 
-        await knex("links").insert(linksInsert);
+    await knex("links").insert(linksInsert)
 
-        const tagsInsert = tags.map(name =>{
-            return{
-                note_id,
-                name,
-                user_id
-            }
-        });
+    const tagsInsert = tags.map(name => {
+      return {
+        note_id,
+        name,
+        user_id
+      }
+    })
 
-        await knex("tags").insert(tagsInsert);
+    await knex("tags").insert(tagsInsert)
 
-        res.json();
-    };
+    response.json()
+  }
 
-    async show(req,res){
-        const {id} = req.params;
+  async show(request, response) {
+    const { id } = request.params
 
-        const note = await knex("notes").where({id}).first();
-        const tags = await knex("tags").where({note_id: id}).orderBy("name");
-        const links = await knex("links").where({note_id: id}).orderBy("created_at");
+    const note = await knex("notes").where({ id }).first()
+    const tags = await knex("tags").where({ note_id: id }).orderBy("name")
+    const links = await knex("links").where({ note_id: id }).orderBy("created_at")
 
+    return response.json({
+      ...note,
+      tags,
+      links
+    })
+  }
 
-        return res.json({
-            ...note,
-            tags,
-            links,
-        });
-     }
+ async delete(req,res){
+  const { id } = req.params;
+  await knex("notes").where({id}).delete();
+  res.json();
+ }
+
+ async index(req,res){
+    const {user_id, title, tags} = req.query;
+    let notes;
+
+    if(tags){
+      const filterTags = tags.split(',').map(tag=> tag.trim());
+
+      notes = await knex("tags")
+      .select([
+        "notes.id",
+        "notes.title",
+        "notes.user_id"
+      ])
+      .where("notes.user_id", user_id)
+      .whereLike("notes.title",`%${title}%`)
+      .innerJoin("notes", "notes.id", "tags.note_id")
+      .whereIn("name", filterTags)
+      .orderBy("notes.title")
+    }else{
+     notes = await knex("notes")
+      .where({user_id}).
+      whereLike("title", `%${title}%`).
+      orderBy("title");
+    }
+
+    const userTags = await knex("tags").where({user_id});
+    const notesWithTags = notes.map(note =>{
+      const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+      return{
+        ...note,
+        tags: noteTags
+      }
+    });
+    return res.json(notesWithTags);
+ }
 }
 
-module.exports = NotesController;
+module.exports = NotesController
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const {Router} = require("express");
 const userRouter = require("./users.routes");
 const notesRouter = require("./notes.routes");
+const tagsRouter = require("./tags.routes")
 
 const router = Router();
 
 router.use("/users", userRouter)
 router.use("/notes", notesRouter)
+router.use("/tags", tagsRouter)
 
 module.exports = router;
