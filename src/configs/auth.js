650ded0185161994fe0fa012591eb4c38d4323a5
Lucas Freitas Marques (CGIS/STIC/UNIC) <lucasfmarques@mec.gov.br>
2024-07-30 14:05:26 -0300

    adicionando autenticador

---
+++ src/configs/auth.js
@@ -0,0 +1,6 @@
+module.exports = {
+    jwt: {
+        secret: "HEST40",
+        expiresIn: "1d"
+    }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const {Router} = require("express");
 const userRouter = require("./users.routes");
 const notesRouter = require("./notes.routes");
-const tagsRouter = require("./tags.routes")
+const tagsRouter = require("./tags.routes");
+const sessionsRoutes = require("./sessions.routes")
 
 const router = Router();
 
 router.use("/users", userRouter)
 router.use("/notes", notesRouter)
 router.use("/tags", tagsRouter)
+router.use("/session", sessionsRoutes)
 
 module.exports = router;
